0afca431a13cde5a237a0781d047a7d1d73ed3ce
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-04-25 11:19:41 +0500

    chore: Updated dependencies

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,10 +4,12 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+	require('../src/components/MyPluginIcon/__stories__/MyPluginIcon.native.stories');
 	require('../src/components/MyPluginIcon/__stories__/MyPluginIcon.stories');
 }
 
 const stories = [
+	'../src/components/MyPluginIcon/__stories__/MyPluginIcon.native.stories',
 	'../src/components/MyPluginIcon/__stories__/MyPluginIcon.stories',
 ];
 
